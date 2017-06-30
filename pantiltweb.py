4d670afcbd8691de4a5cef30bb558ad9d9aa9c0c
Frank van der Stad <fvdstad@oefenweb.nl>
2017-06-30 11:48:18 +0200

    Add Flask-assets to the mix

--- pantiltweb.py
+++ pantiltweb.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
-
-import pantilthat
 from sys import exit
 
 try:
-    from flask import Flask, render_template
+    import pantilthat
 except ImportError:
-    exit("This script requires the flask module\nInstall with: sudo pip install flask")
+    exit("This script requires the pantilthat module\nInstall with: sudo pip install pantilthat')
+
+try:
+    from flask import Flask, render_template
+    from flask_assets import Environment, Bundle
+except ImportError:
+    exit("This script requires the flask module and flask_assets\nInstall with: sudo pip install flask and sudo pip install flask-assets")
 
 app = Flask(__name__)
+assets = Environment(app)
+
+js = Bundle(
+    'assets/jquery-3.2.1.js',
+    filters='jsmin',
+    output='gen/packed.js'
+)
+assets.register('js_all', js)
+
+css = Bundle(
+    'assets/bootstrap.min.css',
+    'assets/bootstrap-theme.min.css',
+    filters='cssmin',
+    output='css/min.css'
+)
+assets.register('css_all', css)
+
 
 @app.route('/')
 def home():
